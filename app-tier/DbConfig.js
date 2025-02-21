902ac6d90c47327b2b5308f4b59082c1d4941a4b
sanstuti jain <sanstuti.jain993@gmail.com>
2025-02-21 13:53:11 +0530

    project

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.c0leak0ce0r0.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Suchi993',
+    DB_DATABASE : 'webappdb'
+});
